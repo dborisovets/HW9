8ade9eca96d122322837df18173bae2feb9e8c7c
DARYA.BARYSAVETS <darya.barysavets@intapp.com>
2022-12-12 15:25:10 +0200

    HW9

---
+++ HW9/List/ProgramList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9.List
+{
+    class ProgramList
+    {
+        private static List<int> wholeNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        private static List<string> words = new List<string>() { "phone", "computer", "plane", "tablet", "light" };
+
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+        }
+
+        private static void Task1()
+        {
+            var n = new List<int> { 1, 2, 3, 4, 5 };
+            var result = n.Where(i => i % 2 == 0).Sum();
+            Console.WriteLine(result);
+        }
+
+        private static void Task2()
+        {
+            Console.WriteLine($"Numbers: {wholeNumbers.Count}");
+        }
+
+        private static void Task3()
+        {
+            foreach (var i in words)
+            {
+                if (i.Length == 5)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static void Task4()
+        {
+            var selectedWords = from a in words
+                                where a.Length == 5
+                                select a;
+            foreach (var i in selectedWords) { Console.WriteLine(i); }
+        }
+    }
+}
---
+++ HW9/Queue and Stack/ProgramQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9.Queue_and_Stack
+{
+    class ProgramQueue
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+
+        private static void Task1()
+        {
+            Queue<int> queue = new Queue<int>();
+            string value = "0";
+            do
+            {
+                value = Console.ReadLine();
+                int result = Int32.Parse(value);
+                queue.Enqueue(result);
+            }
+            while (!"0".Equals(value));
+
+            int maxValue = getMaxValue(queue);
+
+            Console.WriteLine(maxValue);
+
+            queue.Dequeue();
+            maxValue = getMaxValue(queue);
+            Console.WriteLine(maxValue);
+        }
+
+        private static int getMaxValue(Queue<int> queueWithInt)
+        {
+            int maxValue = Int32.MinValue;
+            foreach (int item in queueWithInt)
+            {
+                if (item > maxValue) { maxValue = item; }
+            }
+            return maxValue;
+        }
+
+        private static void Task2()
+        {
+            Stack<char> letters = new Stack<char>();
+            for (int i = 0; i < 3; i++)
+            {
+                string value = Console.ReadLine();
+                char letter = value.ToCharArray()[0];
+                letters.Push(letter);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(letters.Pop());
+            }
+        }
+    }
+}
+
+
+
+
+
